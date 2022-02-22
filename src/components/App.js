c1229391da4a357305c780a9f637a850d8400d34
Ashish Sah <ashishsah173@gmail.com>
2022-02-22 16:40:15 +0530

    Buy And Sell Added

--- src/components/App.js
+++ src/components/App.js
@@ -1,53 +1,119 @@
-import React, { Component } from "react";
-import logo from "../logo.png";
+import React, { useEffect, useState } from "react";
+import Web3 from "web3";
+import Token from "../abis/Token.json";
+import EthSwap from "../abis/EthSwap.json";
+import Navbar from "./Navbar";
+import Main from "./Main";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
+const App = () => {
+  const [account, setAccount] = useState("");
+  const [token, setToken] = useState({});
+  const [ethSwap, setEthSwap] = useState({});
+  const [ethBalance, setEthBalance] = useState("0");
+  const [tokenBalance, setTokenBalance] = useState("0");
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function init() {
+      await loadWeb3();
+      await loadBlockchainData();
+    }
+    init();
+  }, []);
+
+  async function loadBlockchainData() {
+    const web3 = window.web3;
+    const accounts = await web3.eth.getAccounts();
+    setAccount(accounts[0]);
+    const ethBalance = await web3.eth.getBalance(accounts[0]);
+    setEthBalance(ethBalance);
+    const networkId = await web3.eth.net.getId();
+    const tokenData = Token.networks[networkId];
+    if (tokenData) {
+      const token = new web3.eth.Contract(Token.abi, tokenData.address);
+      setToken(token);
+      let tokenBalance = await token.methods.balanceOf(accounts[0]).call();
+      setTokenBalance(tokenBalance.toString());
+    } else {
+      window.alert("Token contract not deployed to detected network.");
+    }
+    const ethSwapData = EthSwap.networks[networkId];
+    if (ethSwapData) {
+      const ethSwap = new web3.eth.Contract(EthSwap.abi, ethSwapData.address);
+      setEthSwap(ethSwap);
+    } else {
+      window.alert("EthSwap contract not deployed to detected network.");
+    }
+    setLoading(false);
+  }
+
+  async function loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert(
+        "Non-Ethereum browser detected. You should consider trying MetaMask!"
+      );
+    }
+  }
+
+  const buyTokens = (etherAmount) => {
+    setLoading(true);
+    ethSwap.methods
+      .buyTokens()
+      .send({ value: etherAmount, from: account })
+      .on("transactionHash", () => {
+        setLoading(false);
+      });
+  };
+
+  const sellTokens = (tokenAmount) => {
+    setLoading(true);
+    token.methods
+      .approve(ethSwap.address, tokenAmount)
+      .send({ from: account })
+      .on("transactionHash", () => {
+        ethSwap.methods
+          .sellTokens(tokenAmount)
+          .send({ from: account })
+          .on("transactionHash", () => {
+            setLoading(false);
+          });
+      });
+  };
+
+  return (
+    <div>
+      <Navbar account={account} />
+      <div className="container-fluid mt-5">
+        <div className="row">
+          <main
+            role="main"
+            className="col-lg-12 ml-auto mr-auto"
+            style={{ maxWidth: "600px" }}
           >
-            Dapp University
-          </a>
-        </nav>
-        <div className="container-fluid mt-5">
-          <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
-              <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
+            <div className="content mr-auto ml-auto">
+              {loading ? (
+                <p id="loader" className="text-center">
+                  Loading...
                 </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN{" "}
-                  <u>
-                    <b>NOW! </b>
-                  </u>
-                </a>
-              </div>
-            </main>
-          </div>
+              ) : (
+                <Main
+                  ethBalance={ethBalance}
+                  tokenBalance={tokenBalance}
+                  buyTokens={buyTokens}
+                  sellTokens={sellTokens}
+                />
+              )}
+            </div>
+          </main>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/BuyForm.js
@@ -0,0 +1,89 @@
+import React, { useState, useRef } from "react";
+
+const BuyForm = ({ buyTokens, ethBalance, tokenBalance }) => {
+  const [output, setOutput] = useState("0");
+  const input = useRef(null);
+
+  return (
+    <form
+      className="mb-3"
+      onSubmit={(e) => {
+        e.preventDefault();
+        const etherAmount = input.current.value.toString();
+        buyTokens(window.web3.utils.toWei(etherAmount, "Ether"));
+      }}
+    >
+      <div>
+        <label className="float-left">
+          <b>Input</b>
+        </label>
+        <span className="float-right text-muted">
+          Balance: {window.web3.utils.fromWei(ethBalance, "Ether")}
+        </span>
+      </div>
+      <div className="input-group mb-4">
+        <input
+          type="text"
+          onChange={() => {
+            const etherAmount = input.current.value.toString();
+            setOutput(etherAmount * 100);
+          }}
+          ref={input}
+          className="form-control form-control-lg"
+          placeholder="0"
+          required
+        />
+        <div className="input-group-append">
+          <div className="input-group-text">
+            <img
+              src={
+                "https://raw.githubusercontent.com/dappuniversity/eth_swap/master/src/eth-logo.png"
+              }
+              height="32"
+              alt=""
+            />
+            &nbsp;&nbsp;&nbsp; ETH
+          </div>
+        </div>
+      </div>
+      <div>
+        <label className="float-left">
+          <b>Output</b>
+        </label>
+        <span className="float-right text-muted">
+          Balance: {window.web3.utils.fromWei(tokenBalance, "Ether")}
+        </span>
+      </div>
+      <div className="input-group mb-2">
+        <input
+          type="text"
+          className="form-control form-control-lg"
+          placeholder="0"
+          value={output}
+          disabled
+        />
+        <div className="input-group-append">
+          <div className="input-group-text">
+            <img
+              src={
+                "https://raw.githubusercontent.com/dappuniversity/eth_swap/master/src/token-logo.png"
+              }
+              height="32"
+              alt=""
+            />
+            &nbsp; DApp
+          </div>
+        </div>
+      </div>
+      <div className="mb-5">
+        <span className="float-left text-muted">Exchange Rate</span>
+        <span className="float-right text-muted">1 ETH = 100 DApp</span>
+      </div>
+      <button type="submit" className="btn btn-primary btn-block btn-lg">
+        SWAP!
+      </button>
+    </form>
+  );
+};
+
+export default BuyForm;
